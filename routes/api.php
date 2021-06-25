111ce20f8c7892ef948534faab8fe957fd4b20f7
GaniAdiW <ganiadiw28@gmail.com>
2021-06-25 23:37:08 +0700

    create new api features: manage guardian, update arra sppt data which contain guardian_id; fix problem when updating one sppt data

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AdministratorController;
 use App\Http\Controllers\Api\AuthenticationController;
 use App\Http\Controllers\Api\FamilyController;
+use App\Http\Controllers\Api\GuardianController;
 use App\Http\Controllers\Api\OwnerController;
 use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\SpptController;
@@ -23,7 +24,6 @@ Route::post('/v1/login', [AuthenticationController::class, 'login']);
 Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'v1'], function () {
     Route::post('/logout', [AuthenticationController::class, 'logout']);
 
-    Route::get('/administrators', [AdministratorController::class, 'index']);
     Route::get('/profile', [ProfileController::class, 'show']);
     Route::patch('/profile/update', [ProfileController::class, 'update']);
 
@@ -40,11 +40,25 @@ Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'v1'], function () {
         Route::get('/{nop}', [SpptController::class, 'showSppt']);
         Route::patch('/update/{nop}', [SpptController::class, 'spptUpdate']);
         Route::post('/mutation', [SpptController::class, 'mutation']);
-        Route::delete('/{id}', [SpptController::class, 'destroy']);
+        Route::delete('/delete/{id}', [SpptController::class, 'destroy']); // OK
+        Route::get('guardian/{guardian_id}', [SpptController::class, 'showSpptByGuardian']); // OK
     });
+
+    Route::get('/guardian', [GuardianController::class, 'index']); // OK
 });
 
-Route::group(['middleware' => ['auth:sanctum', 'role:Super Admin'], 'prefix' => 'v1/administrator'], function () {
-    Route::post('/', [AdministratorController::class, 'store']);
-    Route::delete('/{id}', [AdministratorController::class, 'destroy']);
+Route::group(['middleware' => ['auth:sanctum', 'role:Super Admin'], 'prefix' => 'v1'], function () {
+    Route::get('/administrators', [AdministratorController::class, 'index']);
+    Route::group(['prefix' => 'administrator'], function () {
+        Route::post('/', [AdministratorController::class, 'store']);
+        Route::delete('/{id}', [AdministratorController::class, 'destroy']);
+    });
+
+    Route::group(['prefix' => 'guardian'], function () {
+        Route::post('/', [GuardianController::class, 'createGuardian']); // OK
+        Route::put('/{id}', [GuardianController::class, 'updateGuardian']); // OK
+        Route::delete('/{id}', [GuardianController::class, 'destroy']); // OK
+    });
+
+    Route::patch('/sppt/guardian-update', [SpptController::class, 'updateSpptGuardianId']); // OK
 });
