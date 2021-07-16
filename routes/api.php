e37781136ccd8a13a57a57d69a454ca932fc752f
Gani Adi Wiranata <ganiadiw28@gmail.com>
2021-07-16 09:42:13 +0700

    modify route logout middleware

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/login', [AuthenticationController::class, 'login']);
-Route::post('/logout', [AuthenticationController::class, 'logout']);
 Route::get('/v1/families', [FamilyController::class, 'index']);
 Route::get('/v1/guardians', [GuardianController::class, 'index']);
 
@@ -41,26 +40,28 @@ Route::group(['prefix' => 'v1'], function () {
     });
 });
 
-Route::group(['middleware' => ['auth:sanctum', 'role:super admin|admin'], 'prefix' => 'v1'], function () {
+Route::group(['middleware' => ['auth:sanctum', 'role:super admin|admin']], function () {
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
 
-    Route::get('/profile', [ProfileController::class, 'show']);
-    Route::patch('/profile/update', [ProfileController::class, 'update']);
-
-    Route::group(['prefix' => 'family'], function () {
-        Route::post('/', [FamilyController::class, 'store']);
-        Route::patch('/{id}', [FamilyController::class, 'update']);
+    Route::group(['prefix' => 'v1'], function () {
+        Route::get('/profile', [ProfileController::class, 'show']);
+        Route::patch('/profile/update', [ProfileController::class, 'update']);
+    
+        Route::group(['prefix' => 'family'], function () {
+            Route::post('/', [FamilyController::class, 'store']);
+            Route::patch('/{id}', [FamilyController::class, 'update']);
+        });
+    
+        Route::group(['prefix' => 'sppt'], function () {
+            Route::post('/', [SpptController::class, 'store']);
+            Route::patch('/update/{nop}', [SpptController::class, 'update']);
+            Route::post('/mutation', [SpptController::class, 'mutation']);
+            Route::delete('/delete/{id}', [SpptController::class, 'destroy']);
+            Route::post('/tax-history', [TaxHistoryController::class, 'store']);
+            Route::patch('/tax-history/{id}', [TaxHistoryController::class, 'update']);
+            Route::delete('/tax-history/{id}', [TaxHistoryController::class, 'destroy']);
+        });
     });
-
-    Route::group(['prefix' => 'sppt'], function () {
-        Route::post('/', [SpptController::class, 'store']);
-        Route::patch('/update/{nop}', [SpptController::class, 'update']);
-        Route::post('/mutation', [SpptController::class, 'mutation']);
-        Route::delete('/delete/{id}', [SpptController::class, 'destroy']);
-        Route::post('/tax-history', [TaxHistoryController::class, 'store']);
-        Route::patch('/tax-history/{id}', [TaxHistoryController::class, 'update']);
-        Route::delete('/tax-history/{id}', [TaxHistoryController::class, 'destroy']);
-    });
-
 });
 
 Route::group(['middleware' => ['auth:sanctum', 'role:super admin'], 'prefix' => 'v1'], function () {
